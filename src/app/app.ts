66574c1d3a0182f1eba7fac6df3734ebce17f8cd
Teekay <tk.kayas44@gmail.com>
2026-05-21 12:33:58 +0200

    djs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./navbar/navbar";
+import { Hero } from './hero/hero';
 
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
